626b651b3e1dc4425a1b2b7ee24b44bf5cdcf4c6
Jenom <119628596+shehuj@users.noreply.github.com>
2026-05-18 08:44:15 -0500

    Update main.tf

--- terraform/bootstrap/main.tf
+++ terraform/bootstrap/main.tf
@@ -1,5 +1,3 @@
-data "aws_caller_identity" "current" {}
-
 # GitHub Actions OIDC provider thumbprint
 data "tls_certificate" "github" {
   url = "https://token.actions.githubusercontent.com/.well-known/openid-configuration"
@@ -64,11 +62,115 @@ resource "aws_iam_role" "ci" {
   }
 }
 
-# Broad permissions required for terraform plan/apply across all infra resources.
-# Scope down to least-privilege once the environment stabilises.
-resource "aws_iam_role_policy_attachment" "ci_admin" {
+data "aws_iam_policy_document" "ci_permissions" {
+  statement {
+    sid    = "EC2"
+    effect = "Allow"
+    actions = [
+      "ec2:*",
+      "elasticloadbalancing:*",
+      "autoscaling:*",
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    sid     = "EKS"
+    effect  = "Allow"
+    actions = ["eks:*"]
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "IAM"
+    effect = "Allow"
+    actions = [
+      "iam:*Role*",
+      "iam:*Policy*",
+      "iam:*InstanceProfile*",
+      "iam:*OpenIDConnectProvider*",
+      "iam:GetUser",
+      "iam:ListUsers",
+      "iam:PassRole",
+      "iam:TagRole",
+      "iam:UntagRole",
+      "iam:TagPolicy",
+      "iam:UntagPolicy",
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    sid     = "S3"
+    effect  = "Allow"
+    actions = ["s3:*"]
+    resources = ["*"]
+  }
+
+  statement {
+    sid     = "DynamoDB"
+    effect  = "Allow"
+    actions = ["dynamodb:*"]
+    resources = ["*"]
+  }
+
+  statement {
+    sid     = "KMS"
+    effect  = "Allow"
+    actions = ["kms:*"]
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "CloudWatch"
+    effect = "Allow"
+    actions = [
+      "logs:*",
+      "cloudwatch:*",
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    sid     = "AMP"
+    effect  = "Allow"
+    actions = ["aps:*"]
+    resources = ["*"]
+  }
+
+  statement {
+    sid     = "Grafana"
+    effect  = "Allow"
+    actions = ["grafana:*"]
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "SSO"
+    effect = "Allow"
+    actions = [
+      "sso:*",
+      "sso-admin:*",
+      "identitystore:*",
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "STS"
+    effect = "Allow"
+    actions = [
+      "sts:GetCallerIdentity",
+      "sts:AssumeRole",
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role_policy" "ci" {
   for_each = toset(local.environments)
 
-  role       = aws_iam_role.ci[each.key].name
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+  name   = "observability-ci-permissions"
+  role   = aws_iam_role.ci[each.key].name
+  policy = data.aws_iam_policy_document.ci_permissions.json
 }
